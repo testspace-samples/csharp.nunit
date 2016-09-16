80105364a5e6c995048ea615274372f3896040a5
Mark Underseth <marku@s2technologies.com>
2016-09-16 16:23:24 -0700

    downloading client now

--- desktopbuild.bat
+++ desktopbuild.bat
@@ -6,8 +6,8 @@ msbuild money\cs-money.csproj /p:platform=anycpu /p:configuration=debug /p:runCo
 .\money\packages\OpenCover.4.6.519\tools\OpenCover.Console -target:".\money\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe" -targetargs:"money\bin\Debug\cs-money.dll" -output:"coverage.xml" -filter:"+[*]* -[*]*MoneyTest*" -register:user
 
 REM "after_test:"
-REM curl -fsS -o testspace-windows.zip https://testspace-client.s3.amazonaws.com/testspace-windows.zip 
-REM 7z x -y .\testspace-windows.zip
-REM del testspace-windows.zip
+curl -fsS -o testspace-windows.zip https://testspace-client.s3.amazonaws.com/testspace-windows.zip 
+7z x -y .\testspace-windows.zip
+del testspace-windows.zip
 
 testspace publish analysis.xml [Tests]TestResult.xml coverage.xml "master.desktop"
